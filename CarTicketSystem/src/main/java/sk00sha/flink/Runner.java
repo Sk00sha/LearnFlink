cd88656270de6851c378df70fabbff7289c75699
Sk00sha <filipwagner23@gmail.com>
2025-03-16 20:50:32 +0100

    Added KafkaSource to maven

--- CarTicketSystem/src/main/java/sk00sha/flink/Runner.java
+++ CarTicketSystem/src/main/java/sk00sha/flink/Runner.java
@@ -1,7 +1,19 @@
 package sk00sha.flink;
 
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.connector.kafka.source.KafkaSource;
+import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
+
 public class Runner {
     public static void main(String[] args) {
-        System.out.println("lakjyisugd");
+    }
+    private static KafkaSource<String> createKafkaSource(){
+        return KafkaSource.<String>builder()
+                .setBootstrapServers(brokers)
+                .setTopics("input-topic")
+                .setGroupId("my-group")
+                .setStartingOffsets(OffsetsInitializer.earliest())
+                .setValueOnlyDeserializer(new SimpleStringSchema())
+                .build();
     }
 }
