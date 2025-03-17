797b2e08a62a5612808b8652f558fa7b4c080a30
Sk00sha <filipwagner23@gmail.com>
2025-03-17 17:21:54 +0100

    Added new kafkaSource and dependencies in maven

--- CarTicketSystem/src/main/java/sk00sha/flink/Runner.java
+++ CarTicketSystem/src/main/java/sk00sha/flink/Runner.java
@@ -1,17 +1,38 @@
 package sk00sha.flink;
 
+import org.apache.commons.configuration2.Configuration;
+import org.apache.commons.configuration2.PropertiesConfiguration;
+import org.apache.commons.configuration2.builder.FileBasedConfigurationBuilder;
+import org.apache.commons.configuration2.builder.fluent.Parameters;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 public class Runner {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        Parameters params = new Parameters();
+        FileBasedConfigurationBuilder<PropertiesConfiguration> builder =
+                new FileBasedConfigurationBuilder<>(PropertiesConfiguration.class)
+                        .configure(params.properties().setFileName("config.properties"));
+        Configuration config = builder.getConfiguration();
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        DataStream<String> carDataStream= env.fromSource(createKafkaSource(config), WatermarkStrategy.noWatermarks(),config.getString("app.name"), TypeInformation.of(String.class));
+        carDataStream.print();
+        env.execute();
+
     }
-    private static KafkaSource<String> createKafkaSource(){
+
+    private static KafkaSource<String> createKafkaSource(Configuration config){
         return KafkaSource.<String>builder()
-                .setBootstrapServers(brokers)
-                .setTopics("input-topic")
-                .setGroupId("my-group")
+                .setBootstrapServers(config.getString("kafka.broker"))
+                .setTopics(config.getString("kafka.input.topic"))
+                .setGroupId(config.getString("kafka.group.id"))
                 .setStartingOffsets(OffsetsInitializer.earliest())
                 .setValueOnlyDeserializer(new SimpleStringSchema())
                 .build();
